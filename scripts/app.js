503795ca7574d8c269f70167e339155a5eed2588
Lucas Biazon Palma <lucasbiazonpalma@gmail.com>
2023-11-14 11:18:35 -0300

    Organização and headerNoticias

--- scripts/app.js
+++ scripts/app.js
@@ -3,12 +3,15 @@ const articleNoticias = document.querySelector("#articleNoticias")
 const navBarBtNoticias = document.querySelector("#navBarBtNoticias")
 const search = document.querySelector("#search")
 const searchInput = document.querySelector("#inputSearch")
-let verticalScroll = document.documentElement.scrollTop
+const headerPrincipal = document.querySelector("#headerPrincipal")
+
+
+document.addEventListener("DOMContentLoaded", async () => {
+       newsRelevantes()
+ })
 
 document.addEventListener("scroll", () => {
     let verticalScroll = document.documentElement.scrollTop
-    console.log(verticalScroll)
-    const headerPrincipal = document.querySelector("#headerPrincipal")
     if(verticalScroll >= 150){
         headerPrincipal.classList.add("sticky")
     }else(
@@ -16,18 +19,13 @@ document.addEventListener("scroll", () => {
     )
 })
 
-
 searchInput.addEventListener("focusin", () => {
    document.querySelector("#search hr").style.opacity = "1"
-}) 
+})
 searchInput.addEventListener("blur", () => {
     document.querySelector("#search hr").style.opacity = "0.5"
  })
- 
 
-document.addEventListener("DOMContentLoaded", async () => {
-       newsRelevantes()
- })
 
 // Menu DropDown
 navBarBtNoticias.addEventListener("click", () =>{
@@ -49,9 +47,6 @@ navBarBtNoticias.addEventListener("click", () =>{
 search.addEventListener("submit", (event) =>{ 
     event.preventDefault()
     let busca = searchInput.value
-    console.log(busca)
-    // history.pushState(null, null, busca);
-    window.location.href = busca
 })
 
 
@@ -63,35 +58,58 @@ search.addEventListener("submit", (event) =>{
         })  
         let news = await response.json()
         news = news.results
-
-        let noticia1 = document.querySelector("#newsFirst")
-        noticia1.style.backgroundImage = `url(${news[0].multimedia[0].url})`
-        let title1 = document.querySelector("#title1")
-        title1.innerHTML = `${news[0].title}`
-        for(let index in news){
-            if(index != 10){ 
-                let noticia = document.createElement("div")
-                let noticiaTexto = document.createElement("div")
-                let noticiaTitle = document.createElement("h2")
-                let noticiaDesc = document.createElement("p")
-                let noticiaImg = document.createElement("img")
-
-                noticia.classList.add("noticia")
-                noticiaTexto.classList.add("noticiaTexto")
-
-                noticiaTitle.innerHTML = `${news[index].title}`
-                noticiaDesc.innerHTML = `${news[index].abstract}`
-                noticiaImg.setAttribute('src',`${news[index].multimedia[0].url}`)
-
-                noticia.appendChild(noticiaImg)
-                noticiaTexto.appendChild(noticiaTitle)
-                noticiaTexto.appendChild(noticiaDesc)
-                noticia.appendChild(noticiaTexto)
-                articleNoticias.appendChild(noticia)
-            }else{ break }
-        }
+        console.log(news)
+        NoticiaRelevantes(news)
     }catch{
         console.error(`Download error: ${error.message}`); 
     }
  }
+
+function NoticiaRelevantes(news){
+    for(let index in news)
+        {
+        if(index != 10)
+        { 
+            if(index == 0)
+            {
+                let noticia1 = document.querySelector("#newsFirst")
+                let title1 = document.querySelector("#title1")
+                noticia1.style.backgroundImage = `url(${news[0].multimedia[0].url})`
+                title1.innerHTML = `${news[0].title}`
+            }else if(index == 1)
+             {
+                let noticia2 = document.querySelector("#newsSecond")
+                let title2 = document.querySelector("#title2")
+                noticia2.style.backgroundImage = `url(${news[1].multimedia[0].url})`
+                title2.innerHTML = `${news[1].title}`
+             }else if(index == 2)
+                {
+                    let noticia3 = document.querySelector("#newsThird")
+                    let title3 = document.querySelector("#title3")
+                    noticia3.style.backgroundImage = `url(${news[2].multimedia[0].url})`
+                    title3.innerHTML = `${news[2].title}`
+            }
+
+            let noticia = document.createElement("div")
+            let noticiaTexto = document.createElement("div")
+            let noticiaTitle = document.createElement("h2")
+            let noticiaDesc = document.createElement("p")
+            let noticiaImg = document.createElement("img")
+
+            noticia.classList.add("noticia")
+            noticiaTexto.classList.add("noticiaTexto")
+
+            noticiaTitle.innerHTML = `${news[index].title}`
+            noticiaDesc.innerHTML = `${news[index].abstract}`
+            noticiaImg.setAttribute('src',`${news[index].multimedia[0].url}`)
+
+            noticia.appendChild(noticiaImg)
+            noticiaTexto.appendChild(noticiaTitle)
+            noticiaTexto.appendChild(noticiaDesc)
+            noticia.appendChild(noticiaTexto)
+            articleNoticias.appendChild(noticia)
+            
+        }else{ break }
+    }
+}
 
